f16fece78924a5a288956ad5658eaf6e612db8e9
othmane <cvbnm1682@gmail.com>
2023-06-20 14:47:54 -0700

    Putchar0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,10 @@
 #include "main.h"
 /**
- *main _putchar -print
- *is written by othmane
- *return 0
+ * main - print _putchar
+ * is written by mbah
+ * Return: 0
  */
+
 int main(void)
 {
 _putchar('_');
